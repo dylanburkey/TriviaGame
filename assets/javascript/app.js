92ea7f091813d23716dcbe05486c7ea376c49b3e
dylanburkey <dylanburkey@outlook.com>
2018-05-24 17:02:09 -0400

    Need to get to class

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,9 +20,7 @@ WebFontConfig = {
 $('#trivia').hide();
 $('#end').hide();
 
-//+++++++++++++++++Click Events++++++++++++++++++++++
-//===================================================
-
+// Lazy Click Events
 $(document).on('click', '#newGame', function(e) {
 	game.reset();
 });
@@ -38,16 +36,15 @@ $(document).on('click', '#optionsRadios2', function(e) {
 });
 
 
-//+++++++++++++++++Global variables++++++++++++++++++
-//===================================================
+// Global Vars
 
 var panel = $('#quiz');
+// Lazy Fix
 var userRes = $('#resLoad');
 var countStart = 20;
 
 
-//+++++++++++++++++Trivia Variable.+++++++++++++++++
-//=================================================== 
+// Triva Questions
 var questions = 
 	[
 
@@ -109,10 +106,10 @@ var questions =
 
 	];
 
-//////////// Create object with var game and/////////////////
-var game = {
+// Start Game - Hold in Object
+    var game = {
 	questions: questions,
-	currentQuestion: 0,
+	currentQuestionPH: 0,
 	counter: countStart,
 	correct: 0,
 	incorrect: 0,
@@ -124,84 +121,84 @@ var game = {
 			game.timeOut();//...The time out function is called. 
 		}
 	},
-	questionStart: function() {//This function loas the questions and sets timer
-		timer = setInterval(game.countdown, 1000);//SetInterval Method used to specify 
-		panel.html('<h2 class="text-center">' + questions[this.currentQuestion].question + '</h2>');//loads questions with a h2 tag into the html tag panel instance. 
-		for (var i = 0; i < questions[this.currentQuestion].answer.length; i++) {//this loops through the questions.answer array and appends it to the panel instance.
-			panel.append('<br><label><input type="radio" name="optionsRadios" id="optionsRadios2" value="option2"' + 'data-name="' + questions[this.currentQuestion].answer[i] + '">' + questions[this.currentQuestion].answer[i] + '</label>');
+	questionStart: function() {
+		timer = setInterval(game.countdown, 1000);
+		panel.html('<h2 class="text-center">' + questions[this.currentQuestionPH].question + '</h2>');
+		for (var i = 0; i < questions[this.currentQuestionPH].answer.length; i++) {
+            // Very lazy code
+			panel.append('<label class="question-label"><input type="radio" name="optionsRadios" id="optionsRadios2" value="option2"' + 'data-name="' + questions[this.currentQuestionPH].answer[i] + '">' + questions[this.currentQuestionPH].answer[i] + '</label>');
 		}
 	},
-	nextQuestion: function() {//Here the counter resets and loads the next question.
-		game.counter = countStart;//Create an instance for countStart.
-		$('#timer').html(game.counter);//Load the counter into the html tag.
-		game.currentQuestion++;//Adds 1 question every time this function runs.
-		game.questionStart();//Callback to start new question.
+	nextQuestion: function() {
+		game.counter = countStart;
+		$('#timer').html(game.counter);
+		game.currentQuestionPH++;
+		game.questionStart();
 	},
-	timeOut: function() {//Function for when the clock runs out.
-		clearInterval(timer);//This sets the timer count to 0.
-		$('#timer').html(game.counter);//Load the counter into the html tag.
+	timeOut: function() {
+		clearInterval(timer);
+		$('#timer').html(game.counter)
 
-		panel.html('<h2>Sorry Time is Up Bro!!</h2>');//Load text into html tag
-		panel.append('<h3>Here is The Correct Answer: ' + questions[this.currentQuestion].correctAnswer);//Load text into html tag and the correct answer.
-		panel.append('<img id="giph" src="http://i.giphy.com/3ornjXizVZDbngmjRK.gif"/>');//Same here.
+		panel.html('<h2>Wow!, you managed to fail at that question...</h2>');//Load text into html tag
+		panel.append('<h3>Here is The Correct Answer: ' + questions[this.currentQuestionPH].correctAnswer);//Load text into html tag and the correct answer.
 
-		if (game.currentQuestion === questions.length - 1) {//Subtract one to question array if it exactly equal to current quesion.
+		if (game.currentQuestionPH === questions.length - 1) {//Subtract one to question array if it exactly equal to current quesion.
 			setTimeout(game.results, 4 * 1000);//Than we delay 3 seconds ont he results.
 		} else {
 			setTimeout(game.nextQuestion, 4 * 1000);//Or delay 3 second to go to next question.
 		}
 	},
 	results: function() {
-		$('#trivia').hide();//Hide trivia window 
-		$('#end').show();//show Results window
-		clearInterval(timer);//this sets the timer count to 0.
-		userRes.html('<h2>Great Job Buddy, Lets see your Skills</h2>');//Load text using .html method into window.
+		$('#trivia').hide();
+		$('#end').show();
+        clearInterval(timer);
+        // Ask user for name -- and no time do that
+		userRes.html('<h2>Your about this smart... </h2>');//Load text using .html method into window.
 		userRes.append('<h3>Correct Answers: ' + game.correct + '</h3>');//Use the append method to load the correct answer.
 		userRes.append('<h3>Incorrect Answers: ' + game.incorrect + '</h3>');//Same
 		userRes.append('<h3>Unanswered: ' + (questions.length - (game.incorrect + game.correct)) + '</h3>');//Same
-		userRes.append('<br><button id="newGame">Try Again??</button>');//Create a button in and use the append method to load into page
+		userRes.append('<button id="newGame">Are you a 1 or 0?</button>');//Create a button in and use the append method to load into page
 	},
 	clicked: function(e) {//User click function.
 		clearInterval(timer);//Set timer count to 0
-		if ($(e.target).data("name") === questions[this.currentQuestion].correctAnswer) {//
+		if ($(e.target).data("name") === questions[this.currentQuestionPH].correctAnswer) {//
 			this.answeredCorrectly();
 		} else {
 			this.answeredIncorrectly();
 		}
-	},
-	answeredCorrectly: function() {//Function created for when user answers correctly.
-		clearInterval(timer);//Sets timer count to 0.
-		game.correct++;//Add one count to the correct var.
-		panel.html('<h2>Correct!</h2>');//Load text with .html method into window.
-		panel.append('<img id="giph" src="http://i.giphy.com/26tknCqiJrBQG6bxC.gif"/>');//Same here.
+    },
+    // answer correctly
+	answeredCorrectly: function() {
+		clearInterval(timer);
+		game.correct++;
+		panel.html('<h2>Good for you, you are smart.</h2>');//Load text with .html method into window.
 
-		if (game.currentQuestion === questions.length -1) {
+		if (game.currentQuestionPH === questions.length -1) {
 			setTimeout(game.results, 4 * 1000);
 		} else {
 			setTimeout(game.nextQuestion, 4 * 1000);
 		}
 	},
-	answeredIncorrectly: function() {//Function created for when user answers incorrectly.
-		clearInterval(timer);//Sets time count to 0
-		game.incorrect++;//Adds one count to the incorrect var.
-		panel.html('<h2>Too Bad!</h2>');//Load text with .html method into window.
-		panel.append('<h3>The Correct answer is: ' + questions[game.currentQuestion].correctAnswer + '</h3>');//Load text with .html method into window.
-		panel.append('<img id="giph" src="http://i.giphy.com/3o6gb27m9SE4PPJyFy.gif"/>');////Load text with .html method into window.
+	answeredIncorrectly: function() {
+		clearInterval(timer);
+		game.incorrect++;
+		panel.html('<h2>Too Bad!</h2>');
+		panel.append('<h3>The Correct answer is: ' + questions[game.currentQuestionPH].correctAnswer + '</h3>');//Load text with .html method into window.
 
-		if (game.currentQuestion === questions.length - 1) {//Subtract one to question array if it exactly equal to current quesion.
+		if (game.currentQuestionPH === questions.length - 1) {//Subtract one to question array if it exactly equal to current quesion.
 			setTimeout(game.results, 4 * 1000);//Than we delay 3 seconds ont he results.
 		} else {
 			setTimeout(game.nextQuestion, 4 * 1000);//Or delay 3 second to go to next question.
 		}
 	},
 	reset: function() {//Function that resets game.
-		$('#trivia').show();//Show trivia window.
-		$('#end').hide();//Hide results window.
-		this.currentQuestion = 0;//Sets question to 0.
-		this.counter = countStart;//Sets counter to 0.
-		this.correct = 0;//Sets correct var to 0.
-		this.incorrect = 0;//Sets incorrect to 0.
-		this.questionStart();//Calls questionStart function. 
+		$('#trivia').show();
+		$('#end').hide();
+		this.currentQuestionPH = 0;
+		this.counter = countStart;
+		this.correct = 0;
+		this.incorrect = 0;
+		this.questionStart(); 
 
 	}
 };
